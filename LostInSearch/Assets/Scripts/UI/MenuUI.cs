7d6c7355e7d7db469a598468108214cc7703cbd6
zey <bilgetunca57@gmail.com>
2024-04-08 17:30:00 +0300

    Main Menu Buttons Activated and bugs are fixed

--- LostInSearch/Assets/Scripts/UI/MenuUI.cs
+++ LostInSearch/Assets/Scripts/UI/MenuUI.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject playPanel;
+    public Toggle confirmToggle;
 
-    // Update is called once per frame
     void Update()
     {
-        
+      ConfirmToggle();
     }
+
+    public void OpenGame()
+    {
+         SceneManager.LoadScene("SplashScene");
+    }
+    public void QuitButton()
+    {
+         // save any game data here
+    #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+
+    }
+
+    public void PlusButton()
+    {
+       playPanel.SetActive(true);
+    }
+
+     public void ConfirmToggle()
+    {
+
+        if(confirmToggle.isOn){
+            playPanel.SetActive(true);
+        }
+        else{
+            playPanel.SetActive(false);
+        }
+       
+    }
+
 }
